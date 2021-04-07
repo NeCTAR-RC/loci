8a89fed670746e4f2ac876b1f934959d5e7163b7
Kieran Spear <kispear@gmail.com>
2021-04-07 15:06:11 +1000

    WIP: Simplify virtualenv creation
    Change-Id: Ieacd8922207a44ae5f3596c462cd74e1f9b7f14f

--- scripts/setup_pip.sh
+++ scripts/setup_pip.sh
@@ -3,21 +3,6 @@
 set -ex
 
 
-if [[ "${PYTHON3}" == "no" ]]; then
-    TMP_VIRTUALENV="virtualenv"
-else
-    TMP_VIRTUALENV="python3 -m virtualenv --python=python3"
-fi
-
-# This little dance allows us to install the latest pip
-# without get_pip.py or the python-pip package (in epel on centos)
-if (( $(${TMP_VIRTUALENV} --version | cut -d. -f1) >= 14 )); then
-    SETUPTOOLS="--no-setuptools"
-fi
-if (( $(${TMP_VIRTUALENV} --version | cut -d. -f1) >= 20 )); then
-    SETUPTOOLS="--seed pip --download"
-fi
-
 # virtualenv 16.4.0 fixed symlink handling. The interaction of the new
 # corrected behavior with legacy bugs in packaged virtualenv releases in
 # distributions means we need to hold on to the pip bootstrap installation
@@ -26,11 +11,11 @@ fi
 PIPBOOTSTRAP=/var/lib/pipbootstrap
 
 # Create the boostrap environment so we can get pip from virtualenv
-${TMP_VIRTUALENV} --extra-search-dir=/tmp/wheels ${SETUPTOOLS} ${PIPBOOTSTRAP}
+python3 -m virtualenv --python=python3 ${PIPBOOTSTRAP}
 source ${PIPBOOTSTRAP}/bin/activate
 
 # Install setuptools explicitly required for virtualenv > 20 installation
-pip install --upgrade setuptools
+pip install --upgrade pip setuptools
 
 # Upgrade to the latest version of virtualenv
 pip install --upgrade ${PIP_ARGS} virtualenv
@@ -39,7 +24,7 @@ pip install --upgrade ${PIP_ARGS} virtualenv
 hash -r
 
 # Create the virtualenv with the updated toolchain for openstack service
-virtualenv --seed pip --download /var/lib/openstack
+python3 -m virtualenv --python=python3 /var/lib/openstack
 
 # Deactivate the old bootstrap virtualenv and switch to the new one
 deactivate
